b687ceaf4b7b4436f5dd57b105ddcf49d0f424ee
Cesar Augusto <cesinhalius@hotmail.com>
2025-02-12 00:46:55 -0300

    Adicionando lista de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Task from "./components/Task";
 
-
 function App() {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			title: "Estudar",
+			description:
+				"Estudar programação para se tornar um desenvolvedor full-stack",
+			isCompleted: false,
+		},
+		{
+			id: 2,
+			title: "Estudar Ingles",
+			description:
+				"Estudar Ingles para se tornar um desenvolvedor full-stack",
+			isCompleted: false,
+		},
+		{
+			id: 3,
+			title: "ler um livro",
+			description:
+				"Ler um livro novo",
+			isCompleted: false,
+		},
+	]);
+
 	return (
 		<div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
 			<div className="w-[500px]">
 				<h1 className="text-3xl text-slate-100 font-bold text-center">
 					Gerenciador de Tarefa
 				</h1>
-        <AddTask/>
-        <Task/>
+				<AddTask />
+				<Task tasks={tasks}/>
 			</div>
 		</div>
 	);
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,12 @@
-function Task() {
+function Task(props) {
 	return (
-		<div>
-            <h1>Task</h1>
-        </div>
+		<ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+			{props.tasks.map((task) => (
+				<li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">
+					{task.title}
+				</li>
+			))}
+		</ul>
 	);
 }
 
