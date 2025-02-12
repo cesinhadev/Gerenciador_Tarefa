b14e27e2ca59a638d3e7f333c7e0f7e0855cc212
Cesar Augusto <cesinhalius@hotmail.com>
2025-02-12 00:54:32 -0300

    Adicionando botão de ver detalhes

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,8 +2,13 @@ function Task(props) {
 	return (
 		<ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
 			{props.tasks.map((task) => (
-				<li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">
-					{task.title}
+				<li key={task.id} className="flex gap-2">
+					<button className="text-left w-full bg-slate-400 text-white p-2 rounded-md">
+						{task.title}
+					</button>
+					<button className="bg-slate-400 text-white p-2 rounded-md">
+						Ver Detalhes
+					</button>
 				</li>
 			))}
 		</ul>
