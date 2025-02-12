e711218896253f469a96df16811edc2a0e1dd6e1
Cesar Augusto <cesinhalius@hotmail.com>
2025-02-11 23:19:47 -0300

    Adicionando página inicial do site

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-function App() {
- 
+import AddTask from "./components/AddTask";
+import Task from "./components/Task";
 
-  return (
-    <>
-      <h1>Gerenciador de Tarefa</h1>     
-    </>
-  )
+
+function App() {
+	return (
+		<div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+			<div className="w-[500px]">
+				<h1 className="text-3xl text-slate-100 font-bold text-center">
+					Gerenciador de Tarefa
+				</h1>
+        <AddTask/>
+        <Task/>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
