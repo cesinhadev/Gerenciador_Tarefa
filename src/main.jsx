d86ce8849aed44978dc072e49bbb609e406f45ed
Cesar Augusto <cesinhalius@hotmail.com>
2025-02-13 19:12:16 -0300

    Utilizando a dependencia  React-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import DetailTask from './components/DetailTask.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/detailTask',
+    element: <DetailTask />
+  },
+])
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
