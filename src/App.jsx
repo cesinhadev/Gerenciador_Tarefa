7101e76f45955d0a20a6597c5f788223087e37b7
Cesar Augusto <cesinhalius@hotmail.com>
2025-02-13 00:54:54 -0300

    Adicionando a função de adicionar tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -40,13 +40,23 @@ function App() {
 		setTasks(newTask);
 	}
 
+	function onAddTaskSubmit(title, description) {
+		const newTask = {
+			id: tasks.length + 1,
+			title,
+			description,
+			isCompleted: false,
+		};
+		setTasks([...tasks, newTask]);
+	}
+
 	return (
 		<div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-			<div className="w-[500px]">
+			<div className="w-[500px] space-y-5">
 				<h1 className="text-3xl text-slate-100 font-bold text-center">
 					Gerenciador de Tarefa
 				</h1>
-				<AddTask />
+				<AddTask onAddTaskSubmit={onAddTaskSubmit}/>
 				<Task
 					tasks={tasks}
 					onTaskClick={onTaskClick}
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,40 @@
-function AddTask() {
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+
 	return (
-		<div>
-			<h1>AddTask</h1>
+		<div className="space-y-5 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+			<input
+				type="text"
+				placeholder="Digite um titulo para a tarefa"
+				className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+				value={title}
+				onChange={(event) => setTitle(event.target.value)}
+			/>
+			<input
+				type="text"
+				placeholder="Digite uma descrição para a tarefa"
+				className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+				value={description}
+				onChange={(event) => setDescription(event.target.value)}
+			/>
+			<button
+				onClick={() => {
+					// verifica se possui algo dentro da caixa de texto
+					if (!title.trim() || !description.trim()) {
+						alert("Prencha o titulo e a descrição para continuar");
+					} else {
+						onAddTaskSubmit(title, description);
+						setTitle("");
+						setDescription("");
+					}
+				}}
+				className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+			>
+				Adicionar
+			</button>
 		</div>
 	);
 }
